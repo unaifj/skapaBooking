a9d1aa04ff645e780b36c837ec6137d4ae851af4
aitorcavia <aitor.cavia@opendeusto.es>
2021-04-06 10:47:11 +0200

    Objeto Usuario creado

--- src/main/java/com/decode/objects/Usuario.java
+++ src/main/java/com/decode/objects/Usuario.java
@@ -1,5 +1,51 @@
 package com.decode.objects;
 
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class Usuario {
+	private String nomUsuario;
+	private String correo;
+	private String contrasenya;
+	
+	public Usuario(String nomUsuario, String correo, String contrasenya) {
+		super();
+		this.nomUsuario = nomUsuario;
+		this.correo = correo;
+		this.contrasenya = contrasenya;
+	}
+
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getContrasenya() {
+		return contrasenya;
+	}
+
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nomUsuario=" + nomUsuario + ", correo=" + correo + ", contrasenya=" + contrasenya + "]";
+	}
+	
 
 }
