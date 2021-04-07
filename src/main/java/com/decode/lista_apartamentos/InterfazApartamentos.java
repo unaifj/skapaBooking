5e1dd03dbbac1de7bf93fb0bbf344a89d7f1e74e
jagobajr <jagobagomez@opendeusto.es>
2021-04-07 11:36:18 +0200

    Interfaz de apartamentos falta probar listar apartamentos

--- src/main/java/com/decode/lista_apartamentos/InterfazApartamentos.java
+++ src/main/java/com/decode/lista_apartamentos/InterfazApartamentos.java
@@ -1,46 +1,124 @@
 package com.decode.lista_apartamentos;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+
+import javax.swing.JMenuBar;
+import java.awt.Rectangle;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
 
-public class InterfazApartamentos extends JFrame {
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JSpinner;
+import java.awt.Button;
+import java.awt.Panel;
+import javax.swing.JCheckBox;
+import javax.swing.Box;
+import java.awt.Checkbox;
+import javax.swing.JSlider;
+
+public class InterfazApartamentos extends JFrame  {
+	
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazApartamentos frame = new InterfazApartamentos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	
 	public InterfazApartamentos() {
-	setLayout(null);
+		getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 62));
+		getContentPane().setLayout(null);
+		setBounds(300, 200, 1289, 809);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(1066, 3, -1066, 696);
+		getContentPane().add(scrollPane);
+		
+		JPanel panelNorte = new JPanel();
+		panelNorte.setBounds(0, 3, 1273, 106);
+		getContentPane().add(panelNorte);
+		panelNorte.setBackground(Color.BLUE);
+		panelNorte.setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("SKAPABOOKING.COM");
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblTitulo.setForeground(Color.WHITE);
+		lblTitulo.setBounds(10, 0, 250, 37);
+		panelNorte.add(lblTitulo);
+		
+		JLabel lblMoneda = new JLabel("EUR");
+		lblMoneda.setForeground(Color.WHITE);
+		lblMoneda.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblMoneda.setBounds(765, 8, 46, 24);
+		panelNorte.add(lblMoneda);
+		
+		JLabel lblBandera = new JLabel("New label");
+		lblBandera.setIcon(new ImageIcon("C:\\Users\\mikel\\Downloads\\descarga.png"));
+		lblBandera.setBounds(821, 11, 40, 27);
+		panelNorte.add(lblBandera);
+		
+		JButton btnRegistro = new JButton("Hazte cuenta");
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegistro.setBounds(960, 13, 117, 23);
+		panelNorte.add(btnRegistro);
+		
+		JButton btnLogin = new JButton("Iniciar Sesion");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnLogin.setBounds(1087, 13, 123, 23);
+		panelNorte.add(btnLogin);
+		
+		ImageIcon ico1= new ImageIcon("imagenes/tonyespañol.png");//meter las rutas en la bd
+		
+		ImageIcon img1= new ImageIcon(ico1.getImage().getScaledInstance(lblBandera.getWidth(), lblBandera.getHeight(), Image.SCALE_SMOOTH));
+		lblBandera.setIcon(img1);
+		
+		JPanel panelTitulo = new JPanel();
+		panelTitulo.setBounds(12, 121, 1248, 93);
+		getContentPane().add(panelTitulo);
+		
+		JLabel LabelTitulo = new JLabel("");
+		panelTitulo.add(LabelTitulo);
+		
+		JPanel panelAbajo = new JPanel();
+		panelAbajo.setBounds(12, 253, 1248, 426);
+		getContentPane().add(panelAbajo);
+		
+		JLabel LabelDescripcion = new JLabel("");
+		panelAbajo.add(LabelDescripcion);
+		
 	
-	JLabel lblFoto = new JLabel("New label");
-	lblFoto.setBounds(10, 43, 170, 163);
-	add(lblFoto);
-	
-	JLabel lblNombre = new JLabel("New label");
-	lblNombre.setBounds(199, 29, 269, 28);
-	add(lblNombre);
-	
-	JLabel lblLocalizacion = new JLabel("New label");
-	lblLocalizacion.setBounds(199, 68, 269, 28);
-	add(lblLocalizacion);
-	
-	JLabel lblDescripcion = new JLabel("New label");
-	lblDescripcion.setBounds(197, 117, 271, 99);
-	add(lblDescripcion);
-	
-	JLabel lblPuntuacion = new JLabel("New label");
-	lblPuntuacion.setBounds(570, 36, 54, 51);
-	add(lblPuntuacion);
-	
-	JButton btnReserva = new JButton("Reservar");
-	btnReserva.setBackground(Color.GRAY);
-	btnReserva.setForeground(Color.BLACK);
-	btnReserva.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		}
-	});
-	btnReserva.setBounds(551, 113, 89, 23);
-	add(btnReserva);
-}
+		
+	}
 }
