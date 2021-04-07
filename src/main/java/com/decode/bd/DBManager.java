4eb8fa66454469f34078f7f491930e538ba6a773
aitorcavia <aitor.cavia@opendeusto.es>
2021-04-08 00:46:45 +0200

    Creacion de controlador de bd

--- src/main/java/com/decode/bd/DBManager.java
+++ src/main/java/com/decode/bd/DBManager.java
@@ -1,21 +1,28 @@
 package com.decode.bd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
 import javax.jdo.Transaction;
 
 import com.decode.objects.Usuario;
 
 public class DBManager {
 	
-	PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-	PersistenceManager pm = pmf.getPersistenceManager();
-	Transaction tx = pm.currentTransaction();
+	
 	
 	
 	//INSTALACION DE DATOS
-	public void preparedData () {
+	public void preparedData () throws DBException{
+		
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
 		try {
 			tx.begin();
 			Usuario userA = new Usuario("John", "jhon@gmail.com", "john123");
@@ -33,6 +40,50 @@ public class DBManager {
 			}
 			pm.close();
 		}
+	}
+	
+	//LISTAR USUARIOS DE BD
+	
+	
+	public List<Usuario> listarUsuarios() throws DBException{
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
+		tx.begin();
+		
+		Query<Usuario> query = pm.newQuery("javax.jdo.query.SQL","select * from " + "usuario");
+		query.setClass(Usuario.class);
+			
+		List<Usuario> results = query.executeList();
+		
+		tx.commit();
+		pm.close();
+		return results;
+		
+	}
+	
+	//INSERTAR USUARIO
+	public void insertarUsuario(Usuario user) throws DBException{
+		
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
+		try {
+			tx.begin();
+			
+			pm.makePersistent(user);
+
+			tx.commit();
+			
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		
 	}
 	
 	
