ec2b6db542576e22e726e344a60e1da045a13153
AntonChavarria <anton.chavarria@opendeusto.es>
2021-04-10 11:55:04 +0200

    normativa Covid realizado con QR

--- src/main/java/com/decode/contacto/COVID.java
+++ src/main/java/com/decode/contacto/COVID.java
@@ -3,7 +3,9 @@ package com.decode.contacto;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,66 +17,85 @@ public class COVID extends JFrame{
 	getContentPane().setBackground(Color.BLUE);
 	getContentPane().setLayout(null);
 	
-	JLabel lblNewLabel = new JLabel("NORMATIVA COVID");
-	lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-	lblNewLabel.setForeground(Color.WHITE);
-	lblNewLabel.setBounds(95, 11, 174, 19);
-	getContentPane().add(lblNewLabel);
+	JLabel label1 = new JLabel("NORMATIVA COVID");
+	label1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+	label1.setForeground(Color.WHITE);
+	label1.setBounds(95, 11, 174, 19);
+	getContentPane().add(label1);
 	
-	JLabel lblNewLabel_2 = new JLabel("Para las reservas realizadas el 6 de abril de 2020 o después,");
-	lblNewLabel_2.setForeground(Color.WHITE);
-	lblNewLabel_2.setBounds(41, 75, 303, 14);
-	getContentPane().add(lblNewLabel_2);
+	JLabel label2 = new JLabel("Para las reservas realizadas el 6 de abril de 2020 o después,");
+	label2.setForeground(Color.WHITE);
+	label2.setBounds(41, 75, 303, 14);
+	getContentPane().add(label2);
 	
-	JLabel lblNewLabel_3 = new JLabel("te recomendamos considerar el riesgo asociado al coronavirus");
-	lblNewLabel_3.setForeground(Color.WHITE);
-	lblNewLabel_3.setBounds(41, 91, 301, 14);
-	getContentPane().add(lblNewLabel_3);
+	JLabel label3 = new JLabel("te recomendamos considerar el riesgo asociado al coronavirus");
+	label3.setForeground(Color.WHITE);
+	label3.setBounds(41, 91, 301, 14);
+	getContentPane().add(label3);
 	
-	JLabel lblNewLabel_4 = new JLabel("(COVID-19) y las medidas que han tomado los Gobiernos.");
-	lblNewLabel_4.setForeground(Color.WHITE);
-	lblNewLabel_4.setBounds(41, 110, 294, 14);
-	getContentPane().add(lblNewLabel_4);
+	JLabel label4 = new JLabel("(COVID-19) y las medidas que han tomado los Gobiernos.");
+	label4.setForeground(Color.WHITE);
+	label4.setBounds(41, 110, 294, 14);
+	getContentPane().add(label4);
 	
-	JLabel lblNewLabel_5 = new JLabel("Si no reservas una tarifa flexible, es posible que no puedas ");
-	lblNewLabel_5.setForeground(Color.WHITE);
-	lblNewLabel_5.setBounds(41, 135, 303, 14);
-	getContentPane().add(lblNewLabel_5);
+	JLabel label5 = new JLabel("Si no reservas una tarifa flexible, es posible que no puedas ");
+	label5.setForeground(Color.WHITE);
+	label5.setBounds(41, 135, 303, 14);
+	getContentPane().add(label5);
 	
-	JLabel lblNewLabel_6 = new JLabel("optar a un reembolso. La petición de cancelación la gestionará el ");
-	lblNewLabel_6.setForeground(Color.WHITE);
-	lblNewLabel_6.setBounds(41, 149, 322, 14);
-	getContentPane().add(lblNewLabel_6);
+	JLabel label6 = new JLabel("optar a un reembolso. La petición de cancelación la gestionará el ");
+	label6.setForeground(Color.WHITE);
+	label6.setBounds(41, 149, 322, 14);
+	getContentPane().add(label6);
 	
-	JLabel lblNewLabel_6_1 = new JLabel("alojamiento teniendo en cuenta las condiciones de la reserva");
-	lblNewLabel_6_1.setForeground(Color.WHITE);
-	lblNewLabel_6_1.setBounds(41, 164, 303, 14);
-	getContentPane().add(lblNewLabel_6_1);
+	JLabel label7 = new JLabel("alojamiento teniendo en cuenta las condiciones de la reserva");
+	label7.setForeground(Color.WHITE);
+	label7.setBounds(41, 164, 303, 14);
+	getContentPane().add(label7);
 	
-	JLabel lblNewLabel_6_1_2 = new JLabel("En estos tiempos de incertidumbre, te recomendamos reservar\r\n");
-	lblNewLabel_6_1_2.setForeground(Color.WHITE);
-	lblNewLabel_6_1_2.setBounds(41, 213, 322, 14);
-	getContentPane().add(lblNewLabel_6_1_2);
+	JLabel label8 = new JLabel("En estos tiempos de incertidumbre, te recomendamos reservar\r\n");
+	label8.setForeground(Color.WHITE);
+	label8.setBounds(41, 213, 322, 14);
+	getContentPane().add(label8);
 	
-	JLabel lblNewLabel_6_1_3 = new JLabel("y las leyes de protección del consumidor aplicables. ");
-	lblNewLabel_6_1_3.setForeground(Color.WHITE);
-	lblNewLabel_6_1_3.setBounds(41, 181, 303, 14);
-	getContentPane().add(lblNewLabel_6_1_3);
+	JLabel label9 = new JLabel("y las leyes de protección del consumidor aplicables. ");
+	label9.setForeground(Color.WHITE);
+	label9.setBounds(41, 181, 303, 14);
+	getContentPane().add(label9);
 	
-	JLabel lblNewLabel_1 = new JLabel("opciones con cancelación gratis.Si cambias de planes, podrás\r\n");
-	lblNewLabel_1.setForeground(Color.WHITE);
-	lblNewLabel_1.setBounds(41, 229, 322, 14);
-	getContentPane().add(lblNewLabel_1);
+	JLabel label10 = new JLabel("opciones con cancelación gratis.Si cambias de planes, podrás\r\n");
+	label10.setForeground(Color.WHITE);
+	label10.setBounds(41, 229, 322, 14);
+	getContentPane().add(label10);
 	
-	JLabel lblNewLabel_1_1 = new JLabel("cancelación gratis.");
-	lblNewLabel_1_1.setForeground(Color.WHITE);
-	lblNewLabel_1_1.setBounds(41, 265, 303, 14);
-	getContentPane().add(lblNewLabel_1_1);
+	JLabel label11 = new JLabel("cancelación gratis.");
+	label11.setForeground(Color.WHITE);
+	label11.setBounds(41, 265, 303, 14);
+	getContentPane().add(label11);
 	
-	JLabel lblNewLabel_7 = new JLabel("cancelar sin costes hasta la fecha límite del periodo de");
-	lblNewLabel_7.setForeground(Color.WHITE);
-	lblNewLabel_7.setBounds(41, 248, 303, 14);
-	getContentPane().add(lblNewLabel_7);
+	JLabel label12 = new JLabel("cancelar sin costes hasta la fecha límite del periodo de");
+	label12.setForeground(Color.WHITE);
+	label12.setBounds(41, 248, 303, 14);
+	getContentPane().add(label12);
+	
+	JLabel label13 = new JLabel("A continuación, un codigo QR con la información actualizada\r\n");
+	label13.setForeground(Color.WHITE);
+	label13.setBounds(41, 299, 294, 14);
+	getContentPane().add(label13);
+	
+	JLabel label14 = new JLabel("de la situación actual internacional del COVID-19.\r\n");
+	label14.setForeground(Color.WHITE);
+	label14.setBounds(41, 316, 282, 14);
+	getContentPane().add(label14);
+	
+	JLabel label15 = new JLabel("New label");
+	label15.setBounds(125, 341, 113, 98);
+	getContentPane().add(label15);
 	setBounds(300,250,389,500);
+	
+	
+	ImageIcon ico1= new ImageIcon("img/normativacovid.PNG");               
+	ImageIcon img2= new ImageIcon(ico1.getImage().getScaledInstance(label15.getWidth(), label15.getHeight(), Image.SCALE_SMOOTH));    
+	label15.setIcon(img2);
 	}
 	}
