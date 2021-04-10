dd8a7043f42dfeec75f0980ee4480c901e3c8cfa
jagobajr <jagobagomez@opendeusto.es>
2021-04-10 14:27:27 +0200

    junit tests

---
+++ src/test/java/com/decode/skapaBooking/TestDBManager.java
@@ -0,0 +1,32 @@
+package com.decode.skapaBooking;
+
+import junit.framework.TestCase;
+
+public class TestDBManager extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public final void testInsertarUsuarios(){
+		fail("Not yet implemented");
+	}
+	
+	public final void testListarUsuarios(){
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	public final void testExisteUsuario(){
+		fail("Not yet implemented");
+	}
+	
+	public final void testListarApartamentos(){
+		fail("Not yet implemented");
+	}
+}
