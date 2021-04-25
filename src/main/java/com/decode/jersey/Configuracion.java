1e06192b712a69f274f57242f91117e2dca00180
AntonChavarria <anton.chavarria@opendeusto.es>
2021-04-25 11:35:53 +0200

    creacion de distintas clases para el uso de jersey y cambio de dependecias

---
+++ src/main/java/com/decode/jersey/Configuracion.java
@@ -0,0 +1,10 @@
+package com.decode.jersey;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+ 
+@ApplicationPath("api")
+public class Configuracion extends Application{
+
+}
