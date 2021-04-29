5b31f6896e38f8689af5f7841215123dc4e0aa94
AntonChavarria <anton.chavarria@opendeusto.es>
2021-04-29 11:59:44 +0200

    Creacion de dos objetos con sus correspondientes teses

--- src/test/java/com/decode/skapaBooking/TestPago.java
+++ src/test/java/com/decode/skapaBooking/TestPago.java
@@ -29,18 +29,18 @@ public class TestPago {
 		}
 
 	@Test
-	public void testGetusuarioEmisor() {
+	public void testGetUsuarioEmisor() {
 		Usuario usuarioA = new Usuario("Paco","paco@gmail.com","123");
 		Usuario usuarioB = new Usuario("Lucia","lucia@gmail.com","323");
 		Pago Pago1= new Pago(50,usuarioA,usuarioB);
-		assertEquals(Pago1.getCantidad(),usuarioA);
+		assertEquals(Pago1.getUsuarioEmisor(),usuarioA);
 		}
 
 	@Test
-	public void testGetusuario2() {
+	public void testGetUsuarioReceptor() {
 		Usuario usuarioA = new Usuario("Paco","paco@gmail.com","123");
 		Usuario usuarioB = new Usuario("Lucia","lucia@gmail.com","323");
 		Pago Pago1= new Pago(50,usuarioA,usuarioB);
-		assertEquals(Pago1.getCantidad(),usuarioB);
+		assertEquals(Pago1.getUsuarioReceptor(),usuarioB);
 		}
 }
