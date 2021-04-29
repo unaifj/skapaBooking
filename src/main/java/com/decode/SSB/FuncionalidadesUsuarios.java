418ba1ab548d8479893b51efac17a764aa210d4d
AntonChavarria <anton.chavarria@opendeusto.es>
2021-04-29 12:07:13 +0200

    En el anterior merge branch van las primeras implementaciones de jersey incluyendo dos objetos nuevos y sus correspondientes teses

--- src/main/java/com/decode/SSB/FuncionalidadesUsuarios.java
+++ src/main/java/com/decode/SSB/FuncionalidadesUsuarios.java
@@ -22,7 +22,7 @@ public class FuncionalidadesUsuarios {
         {
             add(new Usuario("Paco", "Paquito2","paco@gmail.com"));
             add(new Usuario("Raul", "Raulito","raul@gmail.com"));
-            add(new Usuario("Pedro", "Pedrosa","pedro@gmail.com"));
+            add(new Usuario("Pedro", "Pedrosa21","pedro@gmail.com"));
         }
     };
  
