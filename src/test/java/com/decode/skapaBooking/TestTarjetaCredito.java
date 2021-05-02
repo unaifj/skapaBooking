91bcd57714d076866ae81888fb2f283f2ada1750
jagobajr <jagobagomez@opendeusto.es>
2021-05-02 09:54:53 +0200

    JUnit objeto

---
+++ src/test/java/com/decode/skapaBooking/TestTarjetaCredito.java
@@ -0,0 +1,50 @@
+package com.decode.skapaBooking;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.decode.objects.TarjetaCredito;
+import com.decode.objects.Usuario;
+
+public class TestTarjetaCredito {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNumTarjeta() {
+		Usuario usuario=new Usuario("carlitos444", "carlitos@gmail.com","1234");
+		TarjetaCredito t= new TarjetaCredito(usuario,5426,1509,778);
+		assertEquals(t.getNumTarjeta(),5426);	
+		}
+
+	@Test
+	public void testGetFecha() {
+		Usuario usuario=new Usuario("carlitos444", "carlitos@gmail.com","1234");
+		TarjetaCredito t= new TarjetaCredito(usuario,5426,1509,778);
+		assertEquals(t.getFecha(),1509);	
+		}
+
+	@Test
+	public void testGetCvv() {
+		Usuario usuario=new Usuario("carlitos444", "carlitos@gmail.com","1234");
+		TarjetaCredito t= new TarjetaCredito(usuario,5426,1509,778);
+		assertEquals(t.getCvv(),778);	
+		}
+	
+	@Test
+	public void testGetUsuario() {
+		Usuario usuario=new Usuario("carlitos444", "carlitos@gmail.com","1234");
+		TarjetaCredito t= new TarjetaCredito(usuario,5426,1509,778);
+		assertEquals(t.getUsuario().getNomUsuario(),"carlitos444");	
+		}
+
+}
