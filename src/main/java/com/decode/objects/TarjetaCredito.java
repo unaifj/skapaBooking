bc1f24ebc738ee0acddca2139b5bf094faf5f112
AntonChavarria <anton.chavarria@opendeusto.es>
2021-05-23 13:47:48 +0200

    Ya funciona la documentacion, primeras clases hechas(Usuario y TarjetaCredito)

--- src/main/java/com/decode/objects/TarjetaCredito.java
+++ src/main/java/com/decode/objects/TarjetaCredito.java
@@ -4,7 +4,11 @@ import javax.jdo.annotations.ForeignKey;
 import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
 import javax.jdo.annotations.PersistenceCapable;
-
+/**
+ * Clase TarjetaCredito clase objeto para las diferentes tarjetas de credito
+ * @author Anton
+ *
+ */
 @PersistenceCapable
 @Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class TarjetaCredito {
@@ -14,7 +18,13 @@ public class TarjetaCredito {
 	private int numTarjeta;
 	private int fecha;
 	private int cvv;
-	
+	/**
+	 * Constructor de Tarjeta de Credito
+	 * @param Obtenemos un usuario de la clase Usuario
+	 * @param Int numTarjeta para saber el numero de la tarjeta
+	 * @param Int fecha para saber la fecha de caducidad de la tarjeta
+	 * @param Int CVV para saber el cvv de la tarjeta de credito
+	 */
 	public TarjetaCredito(Usuario usuario, int numTarjeta, int fecha, int cvv) {
 		super();
 		this.usuario = usuario;
@@ -22,7 +32,9 @@ public class TarjetaCredito {
 		this.fecha = fecha;
 		this.cvv = cvv;
 	}
-	
+	/**
+	 *Constructor vacio de Tarjeta de Credito
+	 */
 	public TarjetaCredito() {
 		super();
 		this.usuario = null;
@@ -30,39 +42,65 @@ public class TarjetaCredito {
 		this.fecha = 0;
 		this.cvv = 0;
 	}
-
+	/**
+	 * Devuelve el usuario de la clase Usuario
+	 * @return Usuario de la clase usuario
+	 */
 	public Usuario getUsuario() {
 		return usuario;
 	}
-
+	/**
+	 * Permite cambiar el usuario de la clase Usuario
+	 * @param  Usuario con el valor del Usuario
+	 */
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-
+	/**
+	 * Devuelve el numero de Tarjeta
+	 * @return numTarjeta Int de la tarjeta de credito
+	 */
 	public int getNumTarjeta() {
 		return numTarjeta;
 	}
-
+	/**
+	 * Permite cambiar el numero de la tarjeta de la clase Tarjeta Credito
+	 * @param  numTarjeta con el numero de Tarjeta de Credito
+	 */
 	public void setNumTarjeta(int numTarjeta) {
 		this.numTarjeta = numTarjeta;
 	}
-
+	/**
+	 * Devuelve la fecha de la clase Tarjeta de Credito
+	 * @return fecha de la clase Tarjeta de Credito
+	 */
 	public int getFecha() {
 		return fecha;
 	}
-
+	/**
+	 * Permite cambiar la fecha de la clase Tarjeta de credito
+	 * @param  fecha Int con la nueva fecha
+	 */
 	public void setFecha(int fecha) {
 		this.fecha = fecha;
 	}
-
+	/**
+	 * Devuelve el cvv de la clase Tarjeta de Credito
+	 * @return Cvv de la clase Tarjeta de Credito
+	 */
 	public int getCvv() {
 		return cvv;
 	}
-
+	/**
+	 * Permite cambiar el cvv de la clase Tarjeta de Credito
+	 * @param  cvv con el valor cvv de la Tarjeta de Credito
+	 */
 	public void setCvv(int cvv) {
 		this.cvv = cvv;
 	}
-
+	/**
+	 * toString de la clase Tarjeta de Credito
+	 */
 	@Override
 	public String toString() {
 		return "TarjetaCredito [usuario=" + usuario + ", numTarjeta=" + numTarjeta + ", fecha=" + fecha + ", cvv=" + cvv
